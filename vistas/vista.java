687ba701869571e1d2ee96f1ada75e6c590645f0
s0fiaa-s <snsandoval@scl.edu.gt>
2025-05-05 23:29:17 -0600

    Add files via upload

---
+++ vistas/vista.java
@@ -0,0 +1,24 @@
+package vistas;
+
+public class vista {
+    public static void banner () {
+        
+    
+    System.out.println("\n\n");
+    System.out.println("Bienvenido a mi sofii Java [sofii versión 1.01]");
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("____         __ _"); 
+    System.out.println("/ ___|  ___  / _(_) __ _"); 
+    System.out.println("\\___ \\ / _ \\| |_| |/ _` |");
+    System.out.println("___) | (_) |  _| | (_| |");
+    System.out.println("|____/ \\___/|_| |_|\\__,_|");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("[?] Selecciona una opción para continuar: ");
+    System.out.println("Proyecto iniciado -->  Mar 18 8:45 am 2025");
+    System.out.println("");
+
+    }
+}
